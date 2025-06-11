ffd905584a8276b346fd4def06936cfb3830554b
mdemartino26 <m.demartino99@gmail.com>
2025-06-11 01:17:49 -0300

    alert

---
+++ js/perdon.js
@@ -0,0 +1,6 @@
+const perdon = document.querySelector(".perdon");
+
+function no(){
+    alert('en serio, tengo sueño y no lo voy a adaptar a mobile ahora, salu2')
+}
+perdon.addEventListener('click', no)
